6723bca33247468fd94861e46edb4990c8981713
조신영(집) <sinyoungcho@naver.com>
2021-01-13 01:39:43 +0900

    2579 수정

--- Solved/Silver/2579.cpp
+++ Solved/Silver/2579.cpp
@@ -1,63 +1,29 @@
-#include <iostream>
-#include <algorithm>
-#include <string.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int climbStairs(int pos, int cont);
+int N, prev_f, cur_f;
+int dp[310];
 
+int main()
+{
+    cin.tie(0),
+    ios_base::sync_with_stdio;
+    
+    cin >> N;
+    for(int i=1; i<N+1; i++){
+        cin >> cur_f;
 
-int N, stair[310];
-int stairMem[310][3];
+        if (i==1) {
+            dp[i] = cur_f;
+        } else if (i==2) {
+            dp[i] = cur_f + dp[1];
+        } else {
+            dp[i] = cur_f + max(dp[i-3]+prev_f, dp[i-2]);
+        }
 
+        prev_f = cur_f;
+    }
 
-int main() {
-	memset(stair, 0, sizeof(stair));
-	int cont = 0;
-
-	cin >> N;
-	for (int i = 1; i < N+1; i++) {
-		cin >> stair[i];
-	}
-
-	for (int i = 0; i < 310; i++) {
-		memset(stairMem[i], -1, sizeof(stairMem[i]));
-	}
-
-
-	cout << climbStairs(0, cont);
-
-
-}
-
-int climbStairs(int pos, int cont) {
-
-	if (stairMem[pos][cont] != -1) {	// if calculated before
-		return stairMem[pos][cont];
-	}
-
-	int ret = 0;
-
-	if (pos > N) {	// out of range
-		ret = 0;
-		stairMem[pos][cont] = ret;
-		return ret;
-	}
-	if (((pos == N - 1) ) && (cont == 2)) {	// can't step on the last stair condition
-		ret = -999999999;
-		stairMem[pos][cont] = ret;
-		return ret; 
-	}
-
-
-
-	if (cont == 2) {
-		ret =  climbStairs(pos + 2, 1) + stair[pos];
-	}
-	else {
-		ret =  max(climbStairs(pos + 1, cont + 1), climbStairs(pos + 2, 1)) + stair[pos];
-	}
-
-	stairMem[pos][cont] = ret;
-	return ret;
-	
+    cout << dp[N];
+    return 0;
 }
