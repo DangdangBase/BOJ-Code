4b3e3ffc5fd611ef724c96c1985e0fc5e512f13d
조신영(집) <sinyoungcho@naver.com>
2021-05-03 00:26:47 +0900

    3066 NlogN LIS

---
+++ Solved/Gold/3066.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#define INF 1987654321
+#define P pair<int, int>
+using namespace std;
+
+
+int T, N, tmp;
+
+int main() {
+    cin.tie(0);
+    ios_base::sync_with_stdio(0);
+
+    cin >> T;
+    while(T--){
+        vector<int> seq;
+        seq.push_back(-INF);
+        int len = 0;
+
+        cin >> N;
+        for(int i=0; i<N; i++){
+            cin >> tmp;
+            if(seq.back() < tmp){
+                seq.push_back(tmp);
+                len++;
+            } else {
+                auto idx = lower_bound(seq.begin(), seq.end(), tmp);
+                *idx = tmp;
+            }
+        }
+
+        cout << len << '\n';
+    }
+    return 0;
+}
